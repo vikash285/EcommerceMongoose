7d5d08c9cc92156833d77d79bc8223c490e8caf5
vikash285 <vikashdhaka285@gmail.com>
2023-06-23 21:29:33 +0530

    user table created

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongodb = require('mongodb')
+const getDb = require('../util/database').getDb
+
+const ObjectId = mongodb.ObjectId
+
+class User {
+  constructor(username, email) {
+    this.name = username,
+    this.email = email
+  }
+
+  save() {
+    const db = getDb()
+    return db.collection('users').insertOne(this)
+  }
+
+  static findById(userId) {
+    const db = getDb()
+    return db.collection('user').findOne({ _id: new ObjectId(userId) })
+  }
+}
+
+module.exports = User;
