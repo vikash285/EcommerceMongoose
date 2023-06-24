6afea0f4d6a49b6b194233455b8a8369c5698263
vikash285 <vikashdhaka285@gmail.com>
2023-06-24 14:42:48 +0530

    cart relations and quantity of cart items

--- app.js
+++ app.js
@@ -21,11 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('6495bff6cbca4de0983bc4d2')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-next()
 });
 
 app.use('/admin', adminRoutes)
--- models/user.js
+++ models/user.js
@@ -4,14 +4,34 @@ const getDb = require('../util/database').getDb
 const ObjectId = mongodb.ObjectId
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username,
-    this.email = email
+    this.email = email,
+    this.cart = cart,
+    this._id = id
   }
 
   save() {
     const db = getDb()
     return db.collection('users').insertOne(this)
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp._id.toString() === product._id.toString()
+    })
+    let newQunatity = 1
+    const updatedCartItems = [...this.cart.items]
+
+    if(cartProductIndex >=0 ) {
+      newQunatity = this.cart.items[cartProductIndex].quantity + 1
+      updatedCartItems[cartProductIndex].quantity = newQunatity
+    } else {
+      updatedCartItems.push({ productId: new ObjectId(product._id), quantity: newQunatity })
+    }
+    const updatedCart = { items: updatedCartItems }
+    const db = getDb()
+    return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart }})
   }
 
   static findById(userId) {
